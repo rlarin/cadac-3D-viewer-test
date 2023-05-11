d0a15981fbe7b5a41c4a7dd38af2d24bd18c7ec7
Rainer <rainerlarin@cadac.com>
2023-05-11 12:58:06 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import {CadacThree} from "ngx-cadac-three-viewer";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'cad-viewer-host';
+  public cadacThreeHandler: CadacThree = new CadacThree();
+
+
+  ngAfterViewInit(): void {
+    this.cadacThreeHandler.createScene();
+
+    this.cadacThreeHandler.toggleOrbitControls(true);
+    this.cadacThreeHandler.setAmbientLight();
+    this.cadacThreeHandler.setAxisHelper(30);
+    const light = this.cadacThreeHandler.setMainDirectionalLight();
+    light.position.set(0, 50, 55);
+    this.cadacThreeHandler.setGridHelper(30, 30);
+
+    const cube = this.cadacThreeHandler.createCube(
+      15, 15, 15, '#f8f8f8', true);
+
+    const sphere = this.cadacThreeHandler.createSphere(
+      8, '#eec63e', true);
+
+
+  }
 }
